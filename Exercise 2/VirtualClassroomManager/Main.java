817e4473e2e74674eb41ba76635d0a8bfe365b9b
LohithaKannan <139571609+LohithaKannan@users.noreply.github.com>
2025-10-01 12:58:35 +0530

    Update Main.java

--- Exercise 2/VirtualClassroomManager/Main.java
+++ Exercise 2/VirtualClassroomManager/Main.java
@@ -1,1 +1,28 @@
+package VirtualClassroomManager;
+
+import VirtualClassroomManager.commands.CommandParser;
+import VirtualClassroomManager.service.ClassroomService;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        ClassroomService service = new ClassroomService();
+        CommandParser parser = new CommandParser(service);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Virtual Classroom Manager (type 'help' for commands)");
+
+        while (true) {
+            System.out.print("> ");
+            String line = br.readLine();
+            if (line == null || line.trim().equalsIgnoreCase("exit")) break;
+            parser.parse(line);
+        }
+
+        System.out.println("Goodbye!");
+    }
+}
 
